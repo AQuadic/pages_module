d202300fbdf9e311fe618aa5b8e55231707cc071
Ahmed Ali <scilton19@gmail.com>
2025-08-26 00:26:17 +0300

    add generated PageDto serialization logic

---
+++ lib/dto/page_dto.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'page_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PageDto _$PageDtoFromJson(Map<String, dynamic> json) => PageDto()
+  ..id = (json['id'] as num?)?.toInt()
+  ..title = json['title'] == null ? null : LangModel.fromJson(json['title'])
+  ..description = json['description'] == null
+      ? null
+      : LangModel.fromJson(json['description'])
+  ..show = (json['show'] as List<dynamic>?)?.map((e) => e as String).toList()
+  ..image = _$JsonConverterFromJson<Map<String, dynamic>, ResponsiveImageModel>(
+    json['image'],
+    const ResponsiveImageModelJsonConv().fromJson,
+  );
+
+Map<String, dynamic> _$PageDtoToJson(PageDto instance) => <String, dynamic>{
+  'id': instance.id,
+  'title': instance.title?.toJson(),
+  'description': instance.description?.toJson(),
+  'show': instance.show,
+  'image': _$JsonConverterToJson<Map<String, dynamic>, ResponsiveImageModel>(
+    instance.image,
+    const ResponsiveImageModelJsonConv().toJson,
+  ),
+};
+
+Value? _$JsonConverterFromJson<Json, Value>(
+  Object? json,
+  Value? Function(Json json) fromJson,
+) => json == null ? null : fromJson(json as Json);
+
+Json? _$JsonConverterToJson<Json, Value>(
+  Value? value,
+  Json? Function(Value value) toJson,
+) => value == null ? null : toJson(value);
